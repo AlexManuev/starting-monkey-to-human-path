81fc2e89eaacb02bf1762c14dcb43eb8090946aa
AlexManuev <smanuev@mail.ru>
2018-09-19 23:44:15 +0400

    Added entry point

---
+++ src/PO61/Manuev/wdad/Application.java
@@ -0,0 +1,10 @@
+package PO61.Manuev.wdad;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("I’m Alexander Manuev, and I’m not a monkey");
+    }
+}
+
