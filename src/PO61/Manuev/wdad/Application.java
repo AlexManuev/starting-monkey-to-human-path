d8ca2f6f9b3f3b6cda01b4f0f713d342e002faa1
AlexManuev <smanuev@mail.ru>
2018-09-20 08:47:18 +0400

    fixed bugs

--- src/PO61/Manuev/wdad/Application.java
+++ src/PO61/Manuev/wdad/Application.java
@@ -4,7 +4,6 @@ public class Application {
 
     public static void main(String[] args) {
 
-        System.out.println("I’m Alexander Manuev, and I’m not a monkey");
+        System.out.println("I’m Alexander Manuev, and I’m not a monkey!");
     }
 }
-
