79beccf90cc914c3599211bc3c055933b275ede1
AlexManuev <smanuev@mail.ru>
2018-09-20 20:03:28 +0400

    in progress

---
+++ src/PO61/Manuev/wdad/learn/xml/XmlTask.java
@@ -0,0 +1,17 @@
+package PO61.Manuev.wdad.learn.xml;
+import java.util.Calendar;
+
+public class XmlTask {
+
+    public int earningsTotal(String officiantSecondName, Calendar calendar) {
+        return 5;
+    }
+
+    public void removeDay(Calendar calendar) {
+
+    }
+
+    public void changeOfficiantName(String oldFirstName, String oldSecondName, String newFirstName, String newSecondName) {
+
+    }
+}
